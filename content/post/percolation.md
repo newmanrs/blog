a0a9f68098b85f2584233e506a4b7302fd5d5e05
newmanrs <newmanrs@umich.edu>
2020-01-20 02:20:30 -0800

    Cleaning up some stray files

--- content/post/percolation.md
+++ content/post/percolation.md
@@ -1,6 +1,7 @@
 ---
 title: "Percolation"
 date: 2018-11-29T19:56:05-08:00
+draft: True
 categories:
 - category
 - subcategory
--- pushtoamazon.sh
+++
@@ -1,3 +0,0 @@
-echo "syncing built files directly to Amazon.  Note that this script neither 1) Builds, or 2) Removes files from the existing build directory, so files from prior builds that have not been overwritten are being staged.  Consider doing rm -r public/, then hugo build in this script for an all in one for safety?  Good thing this is a personal website so all this is irrelevant"
-#s3cmd put -P --recursive ./public/ s3://richmondnewman.com
-s3cmd sync ./public/ s3://richmondnewman.com
