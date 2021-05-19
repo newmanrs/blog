ec1b90a3099b5e6e7d84cb963105d17b2cc5fde8
newmanrs <newmanrs@umich.edu>
2021-05-19 15:28:10 -0700

    social share at top of post was too obnoxious

--- patch_theme.sh
+++
@@ -1,3 +0,0 @@
-# Couldn't get the en.toml in template to work with same tools as the submodules, so I'm writing this patch script to copy a local file from main repo into the submodule.  This is a bit janky.
-
-cp i18n/en.toml themes/hugo-future-imperfect-slim/i18n
--- pushtos3.sh
+++ pushtos3.sh
@@ -1,3 +1,2 @@
-./patch_theme.sh
 hugo
 aws s3 sync public/ s3://richmondnewman.com
